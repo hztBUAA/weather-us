4e93eb988586c104684ebe36f25abd34e0799665
BUAA-makima <1242812772@qq.com>
2024-04-15 22:52:05 +0800

    加入接口, 使用apifox的mock

--- src/views/user/ChangePassword.vue
+++ src/views/user/ChangePassword.vue
@@ -1,4 +1,7 @@
 <script>
+import { changePasswordService } from '@/api/user'
+import { Message } from 'element-ui'
+
 export default {
   data() {
     const patterRule = {
@@ -53,10 +56,11 @@ export default {
   },
   methods: {
     submit() {
-      this.$refs.form.validate(valid => {
+      console.log(this.$refs['form'])
+      this.$refs.form.validate(async(valid) => {
         if (valid) {
-          // todo: 使用接口
-          console.log('更新密码')
+          const result = await changePasswordService(this.form)
+          Message.success(result.msg)
         }
       })
     }
@@ -66,7 +70,15 @@ export default {
 
 <template>
   <el-card>
-    <el-form ref="form" :model="form" label-position="right" label-width="100px" :rules="rules" size="medium" style="width: 400px">
+    <el-form
+      ref="form"
+      :model="form"
+      label-position="right"
+      label-width="100px"
+      :rules="rules"
+      size="medium"
+      style="width: 400px"
+    >
       <el-form-item label="旧密码" prop="oldPassword">
         <el-input v-model="form.oldPassword" show-password placeholder="请输入旧密码" />
       </el-form-item>
@@ -83,6 +95,4 @@ export default {
   </el-card>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,15 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_MOCK_API,
+        logLevel: 'debug',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
+    }
+    // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
