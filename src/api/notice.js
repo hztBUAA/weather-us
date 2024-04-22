bae2de018b4231f4eb91308073cd3c7f41dd0aba
何山“ <21373499@buaa.edu.cn>
2024-04-22 23:24:33 +0800

    优化帖子展示

--- src/api/notice.js
+++ src/api/notice.js
@@ -1,6 +1,6 @@
 import { getData } from './api'
 
 export async function getNoticeCont(id) {
-    const url = 'https://mock.apifox.com/m1/4334156-0-default/notice/content?content=%27%3Cp%3Eaaaa%3C/p%3E%27'
-    return getData(url,id)
+    const url = 'https://mock.apifox.com/m1/4334156-3977343-default/notice/digests?apifoxApiId=167533893&apifoxToken=kBmcKryyUxBSwSWfUSCxu2AhVpYxNrwO'
+    return getData(url, id)
 }
