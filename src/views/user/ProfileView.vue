d7459e87b946e7826ed4ca21d128f64c70d4fec8
BUAA-makima <1242812772@qq.com>
2024-05-07 10:40:20 +0800

    反馈增加回复, 个人信息的电话和邮箱在之后注册使用到的时候再显示

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -1,46 +1,41 @@
 <script>
 import { getInfo } from '@/api/user'
-import { Message } from 'element-ui'
 
 export default {
   data() {
     return {
-      user: {
-        username: '蒋世祺',
-        avatar:
-          'https://jsq-big-event.oss-cn-beijing.aliyuncs.com/4dadcc17-c0da-4efc-975d-13dfc2a1cad8.jpg',
-        phone: 10000,
-        email: ''
-      },
+      defaultAvatar: 'https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png',
+      user: { },
       usernameReadOnly: true
     }
   },
   async mounted() {
     const result = await getInfo()
     this.user = result.data
-    Message.success(result.msg)
+    // Message.success(result.msg)
   }
 }
 </script>
 
 <template>
   <el-card>
-    <el-form>
+    <el-form label-position="right" label-width="60px">
       <el-form-item label="用户名">
         {{ user.username }}
       </el-form-item>
       <el-form-item label="头像">
         <!-- todo: 调用上传文件接口, 对文件的校验 -->
         <el-upload class="avatar-uploader" :show-file-list="false" action="">
-          <img :src="user.avatar" class="avatar" title="更换头像" alt="头像">
+          <img :src="user.avatar || defaultAvatar" class="avatar" title="更换头像" alt="头像">
         </el-upload>
       </el-form-item>
-      <el-form-item label="手机号">
-        {{ user.phone }}
-      </el-form-item>
-      <el-form-item label="邮箱">
-        {{ user.email }}
-      </el-form-item>
+      <!-- todo: 等之后注册加入邮箱或者手机再进行显示 -->
+      <!--      <el-form-item label="手机号">-->
+      <!--        {{ user.phone || "无" }}-->
+      <!--      </el-form-item>-->
+      <!--      <el-form-item label="邮箱">-->
+      <!--        {{ user.email || "无" }}-->
+      <!--      </el-form-item>-->
     </el-form>
   </el-card>
 </template>
