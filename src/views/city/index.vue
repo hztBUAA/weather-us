197fc36e7c33cd01342c78d286116fb029675f86
judger2003 <809697142@qq.com>
2024-05-10 13:07:30 +0800

    城市页面适配移动端-v2：移动端的mb-bottom改小

--- src/views/city/index.vue
+++ src/views/city/index.vue
@@ -414,7 +414,7 @@ export default {
     getWeatherImage(weather) {
       const hour = new Date().getHours()
       // console.log('hour', hour, weather, 'weather')
-      if (hour >= 18) {
+      if (hour >= 18 || hour <= 6) {
         return require('@/assets/night-sunny.png') // 使用@表示src目录
       }
 
@@ -641,7 +641,7 @@ i{
   // background-color: #fff;
   // background-image: url(./images/sunny.png);
   background-size: 100%;
-  height: 20rem;
+  height: 44vh;
   padding: 1rem;
   border-radius: 5px;
   box-shadow: 0 2px 4px rgba(26, 12, 12, 0.1);
@@ -751,15 +751,14 @@ h4{
   }
 
   .cur-list-values > div {
-    margin-bottom: 2vh; // 调整每个子元素的底部间距
+    margin-bottom: 1.5vh; // 调整每个子元素的底部间距
   }
   .cur-left-name{
     margin-top: 3rem;
   }
   .current-panel {
     background-size: 100%;
-    height: 40vh; // 使用相对单位
-    padding: 5vw; // 使用相对单位
+    height: 39vh; // 使用相对单位
     border-radius: 5px;
     box-shadow: 0 2px 4px rgba(26, 12, 12, 0.1);
     background-repeat: no-repeat;
