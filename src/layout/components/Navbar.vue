f35441c0bd165ea4166354f466dd06c0d18a9589
黄振庭 <21371024@buaa.edu.cn>
2024-06-05 14:37:48 +0800

    修复右菜单不显示：NavBar承担导航栏

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -3,38 +3,94 @@
     <!-- <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" /> -->
 
     <!-- <breadcrumb class="breadcrumb-container" /> -->
-    <top-bar />
-    <div class="right-menu">
-      <el-dropdown class="avatar-container" trigger="click">
-        <div class="avatar-wrapper">
-          <el-avatar :src="avatar" />
-          <i class="el-icon-caret-bottom" />
-        </div>
-        <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
-            <el-dropdown-item>
-              Home
-            </el-dropdown-item>
+    <!-- <top-bar /> -->
+    <el-menu mode="horizontal" class="main-menu">
+      <el-menu-item index="1">
+        <router-link to="/dashboard">
+          <el-icon class="el-icon-house" />
+          <span>首页</span>
+        </router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link to="/notice/digests">
+          <el-icon class="el-icon-chat-dot-square" />
+          <span>公告</span>
+        </router-link>
+      </el-menu-item>
+      <el-submenu index="3">
+        <template slot="title">
+          <el-icon class="el-icon-s-check" />
+          <span>个人</span>
+        </template>
+        <el-menu-item index="3-1">
+          <router-link to="/user/profile">
+            <el-icon class="el-icon-s-custom" />
+            <span>用户信息</span>
           </router-link>
-          <el-dropdown-item @click.native="goToUserProfile">
-            个人信息管理
-          </el-dropdown-item>
-          <el-dropdown-item @click.native="goToFeedback">
-            反馈
-          </el-dropdown-item>
-          <a target="_blank" href="https://github.com/hztBUAA/weather-us">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <!-- 使用 v-if 和 v-else 来根据用户登录状态显示不同的选项 -->
-          <el-dropdown-item v-if="isLoggedIn" divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
-          </el-dropdown-item>
-          <el-dropdown-item v-else @click.native="login">
-            <span style="display:block;">Log In</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </div>
+        </el-menu-item>
+        <el-menu-item index="3-2">
+          <router-link to="/user/changePassword">
+            <el-icon class="el-icon-setting" />
+            <span>修改密码</span>
+          </router-link>
+        </el-menu-item>
+        <el-menu-item index="3-3">
+          <router-link to="/user/citySubscribe">
+            <el-icon class="el-icon-location-outline" />
+            <span>灾害订阅</span>
+          </router-link>
+        </el-menu-item>
+        <el-menu-item index="3-4">
+          <router-link to="/user/feedback">
+            <el-icon class="el-icon-s-promotion" />
+            <span>反馈</span>
+          </router-link>
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="4">
+        <router-link to="/city/index">
+          <el-icon class="el-icon-school" />
+          <span>城市</span>
+        </router-link>
+      </el-menu-item>
+      <el-menu-item index="5">
+        <router-link to="/map/index">
+          <el-icon class="el-icon-map-location" />
+          <span>地图</span>
+        </router-link>
+      </el-menu-item>
+      <div class="right-menu">
+        <el-dropdown class="avatar-container" trigger="click">
+          <div class="avatar-wrapper">
+            <el-avatar :src="avatar" />
+            <i class="el-icon-caret-bottom" />
+          </div>
+          <el-dropdown-menu slot="dropdown" class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item>
+                家
+              </el-dropdown-item>
+            </router-link>
+            <el-dropdown-item @click.native="goToUserProfile">
+              个人信息管理
+            </el-dropdown-item>
+            <el-dropdown-item @click.native="goToFeedback">
+              反馈
+            </el-dropdown-item>
+            <a target="_blank" href="https://github.com/hztBUAA/weather-us">
+              <el-dropdown-item>Github</el-dropdown-item>
+            </a>
+            <!-- 使用 v-if 和 v-else 来根据用户登录状态显示不同的选项 -->
+            <el-dropdown-item v-if="isLoggedIn" divided @click.native="logout">
+              <span style="display:block;">Log Out</span>
+            </el-dropdown-item>
+            <el-dropdown-item v-else @click.native="login">
+              <span style="display:block;">Log In</span>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-menu>
   </div>
 </template>
 
@@ -42,13 +98,9 @@
 import { mapGetters } from 'vuex'
 // import Breadcrumb from '@/components/Breadcrumb'
 // import Hamburger from '@/components/Hamburger'
-import TopBar from './TopBar'
 
 export default {
   components: {
-    // Breadcrumb,
-    // Hamburger
-    TopBar
   },
   computed: {
     ...mapGetters([
@@ -151,8 +203,8 @@ export default {
 
         .user-avatar {
           cursor: pointer;
-          width: 40px;
-          height: 40px;
+          // width: 40px;
+          height: 10px;
           border-radius: 10px;
         }
 
@@ -168,8 +220,32 @@ export default {
   }
 }
 
-.custom-submenu {
-  padding-left: 20px; /* 根据需要调整左侧内边距 */
-  padding-right: 20px; /* 根据需要调整右侧内边距 */
+.main-menu {
+  /* display: flex; */
+  /* flex-wrap: wrap; */
+  justify-content: start;
+  padding: 0;
+}
+
+.main-menu .el-menu-item,
+.main-menu .el-submenu {
+  flex: 1;
+  text-align: center;
+  min-width: 120px; /* Set a minimum width for each menu item */
+}
+
+@media (max-width: 768px) {
+  .main-menu {
+    flex-direction: row;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .main-menu .el-menu-item,
+.main-menu .el-submenu {
+  flex: 1;
+  text-align: center;
+  padding: 0px;
+  min-width: 30px; /* Set a minimum width for each menu item */
+}
 }
 </style>
