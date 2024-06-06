6902f3a9bf7b8df36ae38cd9b388aca6eeb47742
judger2003 <809697142@qq.com>
2024-06-06 16:31:30 +0800

    重写route-link ->click.native 避免组件加载问题

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,94 +1,65 @@
-·<template>
+<template>
   <div class="navbar">
-    <!-- <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" /> -->
-
-    <!-- <breadcrumb class="breadcrumb-container" /> -->
-    <!-- <top-bar /> -->
     <el-menu mode="horizontal" class="main-menu">
-      <el-menu-item index="1">
-        <router-link to="/dashboard">
-          <el-icon class="el-icon-house" />
-          <span>首页</span>
-        </router-link>
+      <el-menu-item index="1" @click="navigateTo('/dashboard')">
+        <el-icon class="el-icon-house" />
+        <span>首页</span>
       </el-menu-item>
       <el-submenu v-if="isAdmin" index="cnm">
         <template slot="title">
           <el-icon class="el-icon-s-check" />
           <span>管理员功能</span>
         </template>
-        <el-menu-item index="cnm-1">
-          <router-link to="/admin/user-con">
-            <el-icon class="el-icon-user" />
-            <span>用户管理</span>
-          </router-link>
+        <el-menu-item index="cnm-1" @click="navigateTo('/admin/user-con')">
+          <el-icon class="el-icon-user" />
+          <span>用户管理</span>
         </el-menu-item>
-        <el-menu-item index="cnm-2">
-          <router-link to="/admin/launch-varn">
-            <el-icon class="el-icon-bell" />
-            <span>发布预警</span>
-          </router-link>
+        <el-menu-item index="cnm-2" @click="navigateTo('/admin/launch-varn')">
+          <el-icon class="el-icon-bell" />
+          <span>发布预警</span>
         </el-menu-item>
-        <el-menu-item index="cnm-3">
-          <router-link to="/admin/deal-feedback">
-            <el-icon class="el-icon-edit-outline" />
-            <span>处理反馈</span>
-          </router-link>
+        <el-menu-item index="cnm-3" @click="navigateTo('/admin/deal-feedback')">
+          <el-icon class="el-icon-edit-outline" />
+          <span>处理反馈</span>
         </el-menu-item>
-        <el-menu-item index="cnm-4">
-          <router-link to="/admin/launch-notice">
-            <el-icon class="el-icon-s-opportunity" />
-            <span>发布公告</span>
-          </router-link>
+        <el-menu-item index="cnm-4" @click="navigateTo('/admin/launch-notice')">
+          <el-icon class="el-icon-s-opportunity" />
+          <span>发布公告</span>
         </el-menu-item>
       </el-submenu>
-      <el-menu-item index="2">
-        <router-link to="/notice/digests">
-          <el-icon class="el-icon-chat-dot-square" />
-          <span>公告</span>
-        </router-link>
+      <el-menu-item index="2" @click="navigateTo('/notice/digests')">
+        <el-icon class="el-icon-chat-dot-square" />
+        <span>公告</span>
       </el-menu-item>
       <el-submenu index="3">
         <template slot="title">
           <el-icon class="el-icon-s-check" />
           <span>个人</span>
         </template>
-        <el-menu-item index="3-1">
-          <router-link to="/user/profile">
-            <el-icon class="el-icon-s-custom" />
-            <span>用户信息</span>
-          </router-link>
+        <el-menu-item index="3-1" @click="navigateTo('/user/profile')">
+          <el-icon class="el-icon-s-custom" />
+          <span>用户信息</span>
         </el-menu-item>
-        <el-menu-item index="3-2">
-          <router-link to="/user/changePassword">
-            <el-icon class="el-icon-setting" />
-            <span>修改密码</span>
-          </router-link>
+        <el-menu-item index="3-2" @click="navigateTo('/user/changePassword')">
+          <el-icon class="el-icon-setting" />
+          <span>修改密码</span>
         </el-menu-item>
-        <el-menu-item index="3-3">
-          <router-link to="/user/citySubscribe">
-            <el-icon class="el-icon-location-outline" />
-            <span>灾害订阅</span>
-          </router-link>
+        <el-menu-item index="3-3" @click="navigateTo('/user/citySubscribe')">
+          <el-icon class="el-icon-location-outline" />
+          <span>灾害订阅</span>
         </el-menu-item>
-        <el-menu-item index="3-4">
-          <router-link to="/user/feedback">
-            <el-icon class="el-icon-s-promotion" />
-            <span>反馈</span>
-          </router-link>
+        <el-menu-item index="3-4" @click="navigateTo('/user/feedback')">
+          <el-icon class="el-icon-s-promotion" />
+          <span>反馈</span>
         </el-menu-item>
       </el-submenu>
-
-      <el-menu-item index="4">
-        <router-link to="/city/index">
-          <el-icon class="el-icon-school" />
-          <span>城市</span>
-        </router-link>
+      <el-menu-item index="4" @click="navigateTo('/city/index')">
+        <el-icon class="el-icon-school" />
+        <span>城市</span>
       </el-menu-item>
-      <el-menu-item index="5">
-        <router-link to="/map/index">
-          <el-icon class="el-icon-map-location" />
-          <span>地图</span>
-        </router-link>
+      <el-menu-item index="5" @click="navigateTo('/map/index')">
+        <el-icon class="el-icon-map-location" />
+        <span>地图</span>
       </el-menu-item>
       <div class="right-menu">
         <el-dropdown class="avatar-container" trigger="click">
@@ -97,21 +68,18 @@
             <i class="el-icon-caret-bottom" />
           </div>
           <el-dropdown-menu slot="dropdown" class="user-dropdown">
-            <router-link to="/">
-              <el-dropdown-item>
-                家
-              </el-dropdown-item>
-            </router-link>
-            <el-dropdown-item @click.native="goToUserProfile">
+            <el-dropdown-item @click.native="navigateTo('/dashboard')">
+              家
+            </el-dropdown-item>
+            <el-dropdown-item @click.native="navigateTo('/user/profile')">
               个人信息管理
             </el-dropdown-item>
-            <el-dropdown-item @click.native="goToFeedback">
+            <el-dropdown-item @click.native="navigateTo('/user/feedback')">
               反馈
             </el-dropdown-item>
             <a target="_blank" href="https://github.com/hztBUAA/weather-us">
               <el-dropdown-item>Github</el-dropdown-item>
             </a>
-            <!-- 使用 v-if 和 v-else 来根据用户登录状态显示不同的选项 -->
             <el-dropdown-item v-if="isLoggedIn" divided @click.native="logout">
               <span style="display:block;">Log Out</span>
             </el-dropdown-item>
@@ -127,13 +95,9 @@
 
 <script>
 import { mapGetters } from 'vuex'
-// import Breadcrumb from '@/components/Breadcrumb'
-// import Hamburger from '@/components/Hamburger'
-// import store from '@/store'
 import { getToken } from '@/utils/auth'
+
 export default {
-  components: {
-  },
   data() {
     return {
       isAdminFlag: false
@@ -145,7 +109,6 @@ export default {
       'avatar'
     ]),
     isLoggedIn() {
-      // 检查用户是否已登录
       return this.$store.getters.token !== undefined
     },
     isAdmin() {
@@ -166,30 +129,6 @@ export default {
     }
   },
   methods: {
-
-    // isAdmin() {
-    //   const token = getToken()
-    //   if (token) {
-    //     // const hasGetUserInfo = store.getters.avatar
-    //     // if (!hasGetUserInfo) {
-    //     //   await store.dispatch('user/getInfo')
-    //     // }
-    //     // const role = token === 'true' ? 'admin' : 'user'
-    //     // if (role === 'admin') {
-    //     //   console.log(role, 'role')
-    //     //   return true
-    //     // }
-    //     return true
-    //   }
-    //   return false
-    // },
-    goToUserProfile() {
-    // 导航到个人信息管理的路由
-      this.$router.push('/user/profile')
-    },
-    toggleSideBar() {
-      this.$store.dispatch('app/toggleSideBar')
-    },
     async logout() {
       await this.$store.dispatch('user/logout')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
@@ -203,9 +142,30 @@ export default {
         showClose: true // 是否显示关闭按钮
       })
     },
+    navigateTo(path) {
+      this.$router.push(path)
+    },
+    goToUserProfile() {
+      this.navigateTo('/user/profile')
+    },
+    toggleSideBar() {
+      this.$store.dispatch('app/toggleSideBar')
+    },
+    async logout() {
+      await this.$store.dispatch('user/logout')
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+    },
+    login() {
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      this.$message({
+        message: '请登录您的账号',
+        type: 'warning',
+        duration: 1000,
+        showClose: true
+      })
+    },
     goToFeedback() {
-    // 导航到反馈界面的路由
-      this.$router.push('/user/feedback')
+      this.navigateTo('/user/feedback')
     }
   }
 }
